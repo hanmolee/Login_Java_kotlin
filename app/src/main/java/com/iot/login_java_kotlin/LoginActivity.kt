a9bcb0849c34941f307d5bc2db8eec6767430d22
hanmolee <dlgksah@gmail.com>
2017-06-01 20:07:06 +0900

    아...어렵다
    모르곘다.........
    다시 도전하기로

---
+++ app/src/main/java/com/iot/login_java_kotlin/LoginActivity.kt
@@ -0,0 +1,35 @@
+package com.iot.login_java_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    val SUBMIT : Int = 1000
+    val PASSWORD : String = "12"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        submitBtn.setOnClickListener {
+
+            val Input_PW = fingerprintInput.text//입력받은 비밀번호를 변수에 저장
+
+            if (PASSWORD.equals(Input_PW)){//입력한 비밀번호가 맞다면
+
+                val intent = Intent(this, MainActivity::class.java)
+
+                //Parcelable로 데이터를 넘긴다
+
+
+
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/iot/login_java_kotlin/ParcelableData.kt
@@ -0,0 +1,8 @@
+package com.iot.login_java_kotlin
+
+import android.os.Parcel
+import android.os.Parcelable
+
+/**
+ * Created by User on 2017-06-01.
+ */
