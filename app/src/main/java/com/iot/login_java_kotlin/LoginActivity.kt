307a27156559a534d792b6a6e9247de22f83f385
hanmolee <dlgksah@gmail.com>
2017-06-02 12:41:03 +0900

    Intent로 화면 전환까지만...
    화면 전환 후 계좌(데이터) 보내기
    BankAccount 생성
    MainActivity에 돈 보내는 메서드 추가
    잔액조회 메서드 추가
    
    BankAccount에 isVip = 10000원 이상 존재할때
    FEE(수수료)  무료

--- app/src/main/java/com/iot/login_java_kotlin/LoginActivity.kt
+++ app/src/main/java/com/iot/login_java_kotlin/LoginActivity.kt
@@ -1,15 +1,18 @@
 package com.iot.login_java_kotlin
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.toast
 
+
 class LoginActivity : AppCompatActivity() {
 
     val SUBMIT : Int = 1000
     val PASSWORD : String = "12"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +21,12 @@ class LoginActivity : AppCompatActivity() {
 
         submitBtn.setOnClickListener {
 
+
             val Input_PW = fingerprintInput.text//입력받은 비밀번호를 변수에 저장
 
             if (PASSWORD.equals(Input_PW.toString())){//입력한 비밀번호가 맞다면
 
-                val intent = Intent(this, MainActivity::class.java)
+                val intent = Intent(applicationContext as Context, MainActivity::class.java)
 
                 startActivity(intent)
             }
